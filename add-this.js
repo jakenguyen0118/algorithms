da14571306ff5bf15ac597d23d2dbae88f90f160
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-14 10:49:45 -0400

    moved pseudocode up

--- add-this.js
+++ add-this.js
@@ -2,19 +2,21 @@
  * @param {number} num
  * @return {number}
  */
+
+// turns a number into a string
+// split the number into individual strings
+// get the total value and current value
+// takes one digits total/current value, then adds the total with the NEXT current value
+// if sum is still 2 digits (aka greater than 10) run the function again
+// if not return the sum
+
 let addThis = function (num) {
-    // turns a number into a string
     let number = num.toString()
-    // split the number into individual strings
-    // get the total value and current value
     let sum = number.split("").reduce((total, current) => {
-        // takes one digits total/current value, then adds the total with the NEXT current value
         total+=parseInt(current)
         console.log('total', total, 'current', current)
         return total
     }, 0)
-    // if sum is still 2 digits (aka greater than 10) run the function again
-    // if not return the sum
     return sum >= 10 ? addThis(sum) : sum
 }
 
